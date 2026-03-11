75e0c6887c5f73f645aab2749234e09ec2e51196
sb1618 <sb1618@srmist.edu.in>
2026-03-11 10:20:25 +0530

    UC1: Add welcome message module

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,12 @@
+class check_palindrome {
+    void Greet(){
+        System.out.println("Welcome to palindrome checker Managment System");
+    }
+}
+
+public class Main {
+    public static void main(String[] args){
+        check_palindrome c = new check_palindrome();
+        c.Greet();
+    }
+}
