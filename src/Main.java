cbb515f1b5273540622d560e678f918b66ddfbf2
sb1618 <sb1618@srmist.edu.in>
2026-03-11 10:00:20 +0530

    UC2:Hardcoded palindrome

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class PalindromeChecker{
+    boolean isPalindrome(String str) {
+        int f = 0;
+        int l = str.length() - 1;
+        int count = 0;
+        while (f < l) {
+            if (str.charAt(f) != str.charAt(l)) {
+                return false;
+            }
+            f++;
+            l--;
+        }
+
+        return true;
+    }
+}
+
+public class Main {
+    public static void main(String[] args){
+        PalindromeChecker c = new PalindromeChecker();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter String: ");
+        String str = input.nextLine();
+        System.out.println("Is it a palindrome string? ");
+        System.out.println(c.isPalindrome(str) ? "Yes" : "No");
+    }
+}
