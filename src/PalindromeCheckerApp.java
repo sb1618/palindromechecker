f4647b5d72f7a8517ef3e0eaad4306b73f8f6fc1
sb1618 <sb1618@srmist.edu.in>
2026-03-11 10:40:55 +0530

    UC3: Add welcome message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,12 +1,29 @@
-class check_palindrome {
-    void Greet(){
-        System.out.println("Welcome to palindrome checker Managment System");
+
+import java.util.Scanner;
+
+class PalindromeChecker{
+    boolean isPalindrome(String str) {
+        String a = "";
+        for(int i=str.length()-1;i>=0;i--){
+            a+=str.charAt(i);
+        }
+        if(str.equals(a)){
+            return true;
+        }
+        else{
+            return false;
+        }
+
     }
 }
 
 public class PalindromeCheckerApp {
     public static void main(String[] args){
-        check_palindrome c = new check_palindrome();
-        c.Greet();
+        PalindromeChecker c = new PalindromeChecker();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter String: ");
+        String str = input.nextLine();
+        System.out.println("Is it a palindrome string? ");
+        System.out.println(c.isPalindrome(str) ? "Yes" : "No");
     }
 }
